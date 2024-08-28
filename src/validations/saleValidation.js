1f9322db1f6d40ae77303e08e3d725cdd9dbd7d8
José Guilherme <guilhermedev2002@gmail.com>
2024-08-27 23:55:48 -0300

    fix/ajustando validação do preço

--- src/validations/saleValidation.js
+++ src/validations/saleValidation.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 const saleValidation = yup.object({
   date: yup.string().required(),
-  price: yup.number().email().required(),
+  price: yup.number().required(),
   employee: yup.string().required(),
   products: yup.array().of(yup.string()).required(),
 });
